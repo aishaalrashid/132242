deed5b41df6b286b2e13518e1873c441d008bf80
chaice <chaice.bryan@hotmail.co.uk>
2016-04-03 02:19:22 +0100

    Finished the important functionality of the ant brain. Can now successfully check for syntactical correctness for every instruction. Need to acheive this for a whole document now

--- src/main/java/AntBrain.java
+++ src/main/java/AntBrain.java
@@ -8,12 +8,29 @@ import java.util.regex.Pattern;
  */
 public class AntBrain {
 
-    private Pattern sensePattern = Pattern.compile("Sense\\s(Here|Ahead|LeftAhead|RightAhead)\\s\\d\\s\\d");
-    private Pattern markPattern = Pattern.compile("Mark\\s\\d\\s\\d");
+    private Pattern state = Pattern.compile("([0-9]|[0-9][0-9]|[0-9][0-9][0-9]|[0-9][0-9][0-9][0-9])");
+    private Pattern pathValue = Pattern.compile("[0-5]");
+    private Pattern p = Pattern.compile("[0-9]*");
+    private Pattern lr = Pattern.compile("(Left|Right)");
 
-    private Pattern all = Pattern.compile(sensePattern + "|" + markPattern);
+    private Pattern sensedir = Pattern.compile("(Here|Ahead|LeftAhead|RightAhead)");
+    private Pattern cond = Pattern.compile("(Friend|Foe|FriendWithFood|FoeWithFood|Food|Rock|Marker" + "\\s" + pathValue + "|FowMarker|Home|FoeHome)");
 
-    public boolean check(String input){
+    private Pattern sensePattern = Pattern.compile("Sense\\s" + sensedir + "\\s" + state + "\\s" + state + "\\s" + cond );
+    private Pattern markPattern = Pattern.compile("Mark\\s" + pathValue + "\\s" + state);
+    private Pattern unmarkPattern = Pattern.compile("Unmark\\s" + pathValue + "\\s" + state);
+    private Pattern pickupPattern = Pattern.compile("PickUp\\s" + state + "\\s" + state);
+    private Pattern dropPattern = Pattern.compile("Drop\\s" + state);
+    private Pattern turnPattern = Pattern.compile("Turn\\s" + lr + "\\s" + state);
+    private Pattern movePattern = Pattern.compile("Move\\s" + state + "\\s" + state);
+    private Pattern flipPattern = Pattern.compile("Flip\\s" + p + "\\s" + state + "\\s" + state);
+
+
+
+
+    private Pattern all = Pattern.compile(sensePattern + "|" + markPattern + "|" + unmarkPattern + "|" + pickupPattern + "|" + dropPattern + "|" + turnPattern + "|" + movePattern + "|" + flipPattern);
+
+    public boolean checkInstruction(String input){
         Matcher allMatcher = all.matcher(input);
         return allMatcher.matches();
     }
--- src/test/java/AntBrainTest.java
+++ src/test/java/AntBrainTest.java
@@ -1,10 +1,7 @@
 package test.java;
 
 import junit.framework.TestCase;
-import main.java.Ant;
 import main.java.AntBrain;
-
-import java.util.regex.Pattern;
 
 /**
  * Created by chaic on 31/03/2016.
@@ -15,16 +12,91 @@ public class AntBrainTest extends TestCase {
     public void testCheckSenseInstruction(){
         AntBrain antBrain = new AntBrain();
 
-        String senseInstruction = "Sense Ahead 1 2";
-        boolean result = antBrain.check(senseInstruction);
+        String senseInstruction = "Sense Ahead 1 2 Foe";
+        boolean result = antBrain.checkInstruction(senseInstruction);
         assertTrue(result);
     }
+
+    public void testStateValueLimit(){
+        AntBrain antBrain = new AntBrain();
+
+        String invalidSenseInstruction = "Sense Ahead 1 100000 Foe"; //Exceeds 9999 -> Should fail
+        boolean result = antBrain.checkInstruction(invalidSenseInstruction);
+        assertFalse(result);
+    }
+
+    public void testCondMarker(){
+        AntBrain antBrain = new AntBrain();
+
+        String senseWithMarkerCond = "Sense Ahead 1 4 Marker 3";
+        boolean result = antBrain.checkInstruction(senseWithMarkerCond);
+        assertTrue(result);
+    }
+
 
     public void testCheckMarkInstruction(){
         AntBrain antBrain = new AntBrain();
 
         String markInstruction = "Mark 1 5";
-        boolean result = antBrain.check(markInstruction);
+        boolean result = antBrain.checkInstruction(markInstruction);
+        assertTrue(result);
+    }
+
+    public void testPathValueLimit(){
+        AntBrain antBrain = new AntBrain();
+
+        String markInstruction = "Mark 6 13"; // 6 > max path value
+        boolean result = antBrain.checkInstruction(markInstruction);
+        assertFalse(result);
+    }
+
+    public void testCheckUnMarkInstruction(){
+        AntBrain antBrain = new AntBrain();
+
+        String unmarkInstruction = "Unmark 3 2";
+        boolean result = antBrain.checkInstruction(unmarkInstruction);
+        assertTrue(result);
+    }
+
+    public void testCheckPickUpIntstruction(){
+        AntBrain antBrain = new AntBrain();
+
+        String pickupInstruction = "PickUp 3 4";
+        Boolean result = antBrain.checkInstruction(pickupInstruction);
+        assertTrue(result);
+    }
+
+    public void testCheckDropInstruction(){
+        AntBrain antBrain = new AntBrain();
+
+        String dropInstruction = "Drop 20";
+        boolean result = antBrain.checkInstruction(dropInstruction);
+        assertTrue(result);
+    }
+
+    public void testCheckTurnInstruction(){
+        AntBrain antBrain = new AntBrain();
+
+        String turnInstruction = "Turn Left 60";
+        boolean result = antBrain.checkInstruction(turnInstruction);
+        assertTrue(result);
+    }
+
+    public void testCheckMoveInstruction(){
+        AntBrain antBrain = new AntBrain();
+
+        String moveInstruction = "Move 7 22";
+        boolean result = antBrain.checkInstruction(moveInstruction);
+        assertTrue(result);
+    }
+
+    public void testCheckFlipInstruction(){
+        AntBrain antBrain = new AntBrain();
+
+        String flipInstruction = "Flip 555 92 95";
+        boolean result = antBrain.checkInstruction(flipInstruction);
         assertTrue(result);
     }
 }
+
+
